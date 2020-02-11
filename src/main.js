e3e763a91971907ca95f404c326a9d8cfff30325
Geoff Goetz and Todd Warlraven <geoff@>
2020-02-10 16:32:39 -0800

    Add CSS, Main.js, HTML and Loaders.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-// import $ from 'jquery';
+// import { Tamagotchi } from '../src/tamagotchi'
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function() {
+  $("form#nameInput").submit(function(event) {
+    event.preventDefault();
+    let name = $("input#name").val();
+    console.log(name)
+    $(".main").hide();
+    $(".game").show();
+    $("#background").hide();
+
+  });
+  $("#back").click(function(){
+    $(".main").show();
+    $(".game").hide();
+    $("#background").show();
+  });
+
+});
